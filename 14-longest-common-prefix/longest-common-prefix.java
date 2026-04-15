e03e9242166c82af77db40d2635372c9082af23b
George S Mulbah II 🦀 <51964478+sir-george2500@users.noreply.github.com>
2026-04-15 07:35:21 +0200

    Time: 1 ms (63.31%) | Memory: 43.4 MB (21.18%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+
+        int i = 0;
+
+        String first = strs[0];
+        String last = strs[strs.length-1];
+
+        while(i < first.length() && i < last.length() && first.charAt(i) == last.charAt(i)){
+            i++;
+        }
+
+        return first.substring(0,i);
+    }
+}
