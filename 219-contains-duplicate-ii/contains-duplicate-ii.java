e084e638da1c7d72e51e0fbe6c546b66a5abf1dd
George S Mulbah II 🦀 <51964478+sir-george2500@users.noreply.github.com>
2026-04-26 01:18:26 +0200

    Time: 22 ms (90.85%) | Memory: 90.6 MB (37.23%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> lastSeen = new HashMap<>();
+
+        for(int i = 0; i< nums.length; i++){
+            if(lastSeen.containsKey(nums[i]) && i-lastSeen.get(nums[i])<=k){
+                return true;
+            }
+
+            lastSeen.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
