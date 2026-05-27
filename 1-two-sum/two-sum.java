076352cebb71b83f8de54b30b100dbccc867c924
George S Mulbah II 🦀 <51964478+sir-george2500@users.noreply.github.com>
2026-05-27 23:08:42 +0200

    Time: 3 ms (54.85%) | Memory: 46.9 MB (72.19%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if(nums.length == 0) return new int[]{};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i= 0; i < nums.length; i++){
+            int com = target - nums[i];
+
+            if(map.containsKey(com)){
+                return new int[]{map.get(com), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
