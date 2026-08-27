2591be9ab9dbe54bdc21c9f83c929b1602e87302
George S Mulbah II 🦀 <51964478+sir-george2500@users.noreply.github.com>
2026-08-27 23:22:29 +0200

    Time: 0 ms (100.00%) | Memory: 21.1 MB (40.31%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -1,26 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>> res;
         vector<int> path;
-        dfs(root, targetSum, 0, path, res);
+        dfs(res, path, targetSum,0, root);
         return res;
     }
 
-    void dfs(TreeNode* node, int targetSum, int curSum,
-             vector<int>& path, vector<vector<int>>& res) {
-        if (!node) return;
+    void dfs(vector<vector<int>> &res, vector<int> &path, int targetSum, int currSum, TreeNode* node){
 
-        curSum += node->val;
+        if(!node) return;
+        
+        currSum += node->val;
         path.push_back(node->val);
+        // base case
+        if(!node->left && !node->right && currSum == targetSum) res.push_back(path);
 
-        if (!node->left && !node->right && curSum == targetSum) {
-            res.push_back(path);
-        }
-
-        dfs(node->left, targetSum, curSum, path, res);
-        dfs(node->right, targetSum, curSum, path, res);
+        if(node->left) dfs(res,path, targetSum, currSum, node->left);
+        if(node->right) dfs(res,path, targetSum, currSum, node->right);
 
         path.pop_back();
+
     }
 };
