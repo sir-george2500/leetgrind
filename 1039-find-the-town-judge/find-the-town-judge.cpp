960110065f7e4ec5005531fbd39f4b43446cf4f0
George S Mulbah II 🦀 <51964478+sir-george2500@users.noreply.github.com>
2026-09-01 18:06:03 +0200

    Time: 0 ms (100.00%) | Memory: 65.1 MB (83.71%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> indegree(n+1, 0);
         vector<int> outdegree(n+1, 0);
+        vector<int> indegree(n+1, 0);
 
         for(auto& edge:trust){
             int a = edge[0];
             int b = edge[1];
 
-            // a on b
             outdegree[a]++;
             indegree[b]++;
         }
 
-        int ans = 0;
-
         for(int p = 1; p <=n; p++){
-            if(outdegree[p] == 0 && indegree[p] == n-1){
-                ans = p;
-                return ans;
-            }
+            if(indegree[p] == n-1 && outdegree[p] == 0) return p;
         }
 
         return -1;
